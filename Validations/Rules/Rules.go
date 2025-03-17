7d6ce95dd6a66d48e9e41c4680af0acfe284642c
mahdic200 <rmahdi170@gmail.com>
2025-03-17 17:33:47 +0330

    added validation rules

--- Validations/Rules/Rules.go
+++ Validations/Rules/Rules.go
@@ -9,9 +9,15 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/mahdic200/weava/Config"
 	"github.com/mahdic200/weava/Providers/Validation"
+	"github.com/mahdic200/weava/Services/FileService"
 )
 
 type ValidationRule func(c *fiber.Ctx, field_name string) (passed bool, message string, flags map[string]bool, err error)
+
+type FieldRules struct {
+    FieldName string
+    Rules []ValidationRule
+}
 
 
 /* general validation rules */
@@ -36,7 +42,6 @@ func Sometimes(c *fiber.Ctx, field_name string) (passed bool, message string, fl
     if err != nil {
         return false, "", nil, err
     }
-    fmt.Printf("value : %#v\n", value)
     flags = make(map[string]bool)
     flags["itsnull"] = true
     if value == nil {
@@ -53,11 +58,12 @@ func Required(c *fiber.Ctx, field_name string) (passed bool, message string, fla
         return false, "", nil, err
     }
     value := form.Value[field_name]
+    file := form.File[field_name]
     message, err = Validation.ErrorMessageProvider(field_name, "required", nil)
     if err != nil {
         return false, "", nil, err
     }
-    if value == nil || value[0] == "" {
+    if (value == nil || value[0] == "") && (file == nil) {
         return false, message, nil, nil
     }
     return true, "", nil, nil
@@ -131,6 +137,71 @@ func NotInArray(array []string) ValidationRule {
 
 /* string validation rules */
 
+func Length(length uint) ValidationRule {
+    return func(c *fiber.Ctx, field_name string) (passed bool, message string, flags map[string]bool, err error) {
+        value := c.FormValue(field_name)
+        otherKeys := make(map[string]string)
+        otherKeys["{length}"] = fmt.Sprintf("%v", length)
+        message, err = Validation.ErrorMessageProvider(field_name, "length", otherKeys)
+        if err != nil {
+            return false, "", nil, err
+        }
+        if len(value) != int(length) {
+            return false, message, nil, nil
+        }
+        return true, "", nil, nil
+    }
+}
+
+func MinLength(min uint) ValidationRule {
+    return func(c *fiber.Ctx, field_name string) (passed bool, message string, flags map[string]bool, err error) {
+        value := c.FormValue(field_name)
+        otherKeys := make(map[string]string)
+        otherKeys["{min}"] = fmt.Sprintf("%v", min)
+        message, err = Validation.ErrorMessageProvider(field_name, "minLength", otherKeys)
+        if err != nil {
+            return false, "", nil, err
+        }
+        if len(value) < int(min) {
+            return false, message, nil, nil
+        }
+        return true, "", nil, nil
+    }
+}
+
+func MaxLength(max uint) ValidationRule {
+    return func(c *fiber.Ctx, field_name string) (passed bool, message string, flags map[string]bool, err error) {
+        value := c.FormValue(field_name)
+        otherKeys := make(map[string]string)
+        otherKeys["{max}"] = fmt.Sprintf("%v", max)
+        message, err = Validation.ErrorMessageProvider(field_name, "maxLength", otherKeys)
+        if err != nil {
+            return false, "", nil, err
+        }
+        if len(value) > int(max) {
+            return false, message, nil, nil
+        }
+        return true, "", nil, nil
+    }
+}
+
+func LengthBetween(min uint, max uint) ValidationRule {
+    return func(c *fiber.Ctx, field_name string) (passed bool, message string, flags map[string]bool, err error) {
+        value := c.FormValue(field_name)
+        otherKeys := make(map[string]string)
+        otherKeys["{min}"] = fmt.Sprintf("%v", min)
+        otherKeys["{max}"] = fmt.Sprintf("%v", max)
+        message, err = Validation.ErrorMessageProvider(field_name, "lengthBetween", otherKeys)
+        if err != nil {
+            return false, "", nil, err
+        }
+        if len(value) > int(max) || len(value) < int(min) {
+            return false, message, nil, nil
+        }
+        return true, "", nil, nil
+    }
+}
+
 func Email(c *fiber.Ctx, field_name string) (passed bool, message string, flags map[string]bool, err error) {
     value := c.FormValue(field_name)
     r := regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
@@ -138,10 +209,134 @@ func Email(c *fiber.Ctx, field_name string) (passed bool, message string, flags 
     if err != nil {
         return false, "", nil, err
     }
-    fmt.Printf("%#v\n", value)
-    fmt.Printf("%#v\n", r.MatchString(value))
     if !r.MatchString(value) {
         return false, message, nil, nil
     }
     return true, "", nil, nil
+}
+
+func Regex(regex string) ValidationRule {
+    return func(c *fiber.Ctx, field_name string) (passed bool, message string, flags map[string]bool, err error) {
+        value := c.FormValue(field_name)
+        r := regexp.MustCompile(regex)
+        message, err = Validation.ErrorMessageProvider(field_name, "regex", nil)
+        if err != nil {
+            return false, "", nil, err
+        }
+        if !r.MatchString(value) {
+            return false, message, nil, nil
+        }
+        return true, "", nil, nil
+    }
+}
+
+/* file validation */
+
+func File(c *fiber.Ctx, field_name string) (passed bool, message string, flags map[string]bool, err error) {
+    form, err := c.MultipartForm()
+    if err != nil {
+        return false, "", nil, fmt.Errorf("Error , File Rule : %s", err)
+    }
+    message, err = Validation.ErrorMessageProvider(field_name, "file", nil)
+    if form.File[field_name] == nil {
+        return false, message, nil, nil
+    }
+    return true, "", nil, nil
+}
+
+func Mimes(array ...string) ValidationRule {
+    return func(c *fiber.Ctx, field_name string) (passed bool, message string, flags map[string]bool, err error) {
+        value, err := c.FormFile(field_name)
+        if err != nil {
+            return false, "", nil, fmt.Errorf("Error , Mines Rule : %s", err)
+        }
+        otherKeys := make(map[string]string)
+        otherKeys["{mimes}"] = strings.Join(array, " , ")
+        message, err = Validation.ErrorMessageProvider(field_name, "mimes", otherKeys)
+        if err != nil {
+            return false, "", nil, err
+        }
+        if !slices.Contains(array, FileService.GetFileExtension(value.Filename)) {
+            return false, message, nil, nil
+        }
+        return true, "", nil, nil
+    }
+}
+
+func FileSize(size uint) ValidationRule {
+    return func(c *fiber.Ctx, field_name string) (passed bool, message string, flags map[string]bool, err error) {
+        value, err := c.FormFile(field_name)
+        if err != nil {
+            return false, "", nil, fmt.Errorf("Error , FileSize Rule : %s", err)
+        }
+        otherKeys := make(map[string]string)
+        otherKeys["{size}"] = fmt.Sprintf("%v", size)
+        message, err = Validation.ErrorMessageProvider(field_name, "fileSize", otherKeys)
+        if err != nil {
+            return false, "", nil, err
+        }
+        if int(value.Size / 1000) != int(size) {
+            return false, message, nil, nil
+        }
+        return true, "", nil, nil
+    }
+}
+
+func MinSize(min uint) ValidationRule {
+    return func(c *fiber.Ctx, field_name string) (passed bool, message string, flags map[string]bool, err error) {
+        value, err := c.FormFile(field_name)
+        if err != nil {
+            return false, "", nil, fmt.Errorf("Error , MinSize Rule : %s", err)
+        }
+        otherKeys := make(map[string]string)
+        otherKeys["{min}"] = fmt.Sprintf("%v", min)
+        message, err = Validation.ErrorMessageProvider(field_name, "minSize", otherKeys)
+        if err != nil {
+            return false, "", nil, err
+        }
+        if int(value.Size / 1000) < int(min) {
+            return false, message, nil, nil
+        }
+        return true, "", nil, nil
+    }
+}
+
+func MaxSize(max uint) ValidationRule {
+    return func(c *fiber.Ctx, field_name string) (passed bool, message string, flags map[string]bool, err error) {
+        value, err := c.FormFile(field_name)
+        if err != nil {
+            return false, "", nil, fmt.Errorf("Error , MaxSize Rule : %s", err)
+        }
+        otherKeys := make(map[string]string)
+        otherKeys["{max}"] = fmt.Sprintf("%v", max)
+        message, err = Validation.ErrorMessageProvider(field_name, "maxSize", otherKeys)
+        if err != nil {
+            return false, "", nil, err
+        }
+        if int(value.Size / 1000) > int(max) {
+            return false, message, nil, nil
+        }
+        return true, "", nil, nil
+    }
+}
+
+func SizeBetween(min uint, max uint) ValidationRule {
+    return func(c *fiber.Ctx, field_name string) (passed bool, message string, flags map[string]bool, err error) {
+        value, err := c.FormFile(field_name)
+        if err != nil {
+            return false, "", nil, fmt.Errorf("Error , SizeBetween Rule : %s", err)
+        }
+        otherKeys := make(map[string]string)
+        otherKeys["{min}"] = fmt.Sprintf("%v", min)
+        otherKeys["{max}"] = fmt.Sprintf("%v", max)
+        message, err = Validation.ErrorMessageProvider(field_name, "sizeBetween", otherKeys)
+        if err != nil {
+            return false, "", nil, err
+        }
+        file_size := value.Size / 1000
+        if int(file_size) > int(max) || int(file_size) < int(min) {
+            return false, message, nil, nil
+        }
+        return true, "", nil, nil
+    }
 }
