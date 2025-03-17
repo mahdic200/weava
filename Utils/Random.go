de70e0dcf6077e4ebdd7735902c7c31a3d063cee
mahdic200 <rmahdi170@gmail.com>
2025-03-17 17:39:02 +0330

    changed random helper

--- Utils/Random.go
+++ Utils/Random.go
@@ -5,18 +5,28 @@ import (
 	"time"
 )
 
-const charset = "abcdefghijklmnopqrstuvwxyz" +
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789" +
+const std_charset = "abcdefghijklmnopqrstuvwxyz" +
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+const charset = std_charset +
     "~!@#$%^&*(){}[]_+=-/`'\\\"><|"
 
 var seededRand *rand.Rand = rand.New(
     rand.NewSource(time.Now().UnixNano()),
 )
 
-func RandomString(length int) string {
+func randomString(length int, charset string) string {
     b := make([]byte, length)
     for i := range b {
         b[i] = charset[seededRand.Intn(len(charset))]
     }
     return string(b)
+}
+
+func RandomString(length int) string {
+    return randomString(length, charset)
+}
+
+func StandardRandomString(length int) string {
+    return randomString(length, std_charset)
 }
