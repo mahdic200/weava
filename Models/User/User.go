12db90df3f0d3af8d59c9744c64b64523436690f
mahdic200 <rmahdi170@gmail.com>
2025-03-18 13:54:06 +0330

    Added Create function for user model

---
+++ Models/User/User.go
@@ -0,0 +1,40 @@
+package User
+
+import (
+	"fmt"
+	"slices"
+	"strings"
+	"time"
+
+	"gorm.io/gorm"
+)
+
+var fillable = []string{
+    "first_name",
+    "last_name",
+    "email",
+    "phone",
+    "image",
+    "password",
+};
+
+func Create(tx *gorm.DB, args map[string]string) *gorm.DB {
+    fields := []string{}
+    question_marks := []string{}
+    validated_args := []any{}
+    for key, value := range args {
+        if slices.Contains(fillable, key) {
+            fields = append(fields, key)
+            question_marks = append(question_marks, "?")
+            validated_args = append(validated_args, value)
+        }
+    }
+    fields = append(fields, "created_at")
+    question_marks = append(question_marks, "?")
+    validated_args = append(validated_args, time.Now())
+
+    query := fmt.Sprintf("INSERT INTO users (%s) VALUES (%s)", strings.Join(fields, ", "), strings.Join(question_marks, ", "))
+    tx = tx.Exec(query, validated_args...)
+
+    return tx
+}
