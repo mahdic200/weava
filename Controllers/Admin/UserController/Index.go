540549f0e11482c6cfdd0905275562561dec81dd
mahdic200 <rmahdi170@gmail.com>
2025-03-22 17:50:12 +0330

    Updated UserController index method

--- Controllers/Admin/UserController/Index.go
+++ Controllers/Admin/UserController/Index.go
@@ -2,7 +2,7 @@ package UserController
 
 import (
 	"github.com/gofiber/fiber/v2"
-	db "github.com/mahdic200/weava/Config"
+	"github.com/mahdic200/weava/Config"
 	"github.com/mahdic200/weava/Models"
 	"github.com/mahdic200/weava/Models/User"
 	"github.com/mahdic200/weava/Resources/UserResource"
@@ -11,18 +11,13 @@ import (
 
 func Index(c *fiber.Ctx) error {
     var users []Models.User
-    tx := db.DB.Table("users")
+    tx := Config.DB.Table("users").Order("id")
+    tx.Order("id")
     var metadata Http.PaginationMetadata
-    if Http.GetQueryArg(c, "page") != nil {
-        tx, metadata = User.Paginate(tx, c)
-        tx.Find(&users)
-        return c.Status(200).JSON(fiber.Map{
-            "data": UserResource.Collection(users),
-            "metadata": metadata,
-        })
-    }
+    tx, metadata = User.Paginate(tx, c)
     tx.Find(&users)
     return c.Status(200).JSON(fiber.Map{
         "data": UserResource.Collection(users),
+        "metadata": metadata,
     })
 }
