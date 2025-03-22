8b37bd57a6bf3a8bda0d983983afb5c88e3efd82
mahdic200 <rmahdi170@gmail.com>
2025-03-22 17:49:21 +0330

    Updated UserValidation

--- Validations/Admin/UserValidation/UserValidation.go
+++ Validations/Admin/UserValidation/UserValidation.go
@@ -35,8 +35,31 @@ func Store() func(c *fiber.Ctx) error {
     })
 }
 
-func UpdateValidator() [](func()) {
-    return [](func()){
-        
-    }
+func Update() func(c *fiber.Ctx) error {
+    return Middlewares.ValidationMiddleware([]Rules.FieldRules{
+        {
+            FieldName: "first_name",
+            Rules: []Rules.ValidationRule{ Rules.Sometimes, Rules.LengthBetween(2, 255) },
+        },
+        {
+            FieldName: "last_name",
+            Rules: []Rules.ValidationRule{ Rules.Sometimes, Rules.LengthBetween(2, 255) },
+        },
+        {
+            FieldName: "image",
+            Rules: []Rules.ValidationRule{ Rules.Sometimes, Rules.File, Rules.Mimes("jpg", "jpeg", "png"), Rules.MaxSize(1000) },
+        },
+        {
+            FieldName: "email",
+            Rules: []Rules.ValidationRule{ Rules.Sometimes, Rules.Email, Rules.LengthBetween(2, 255), Rules.Unique("email", "users") },
+        },
+        {
+            FieldName: "phone",
+            Rules: []Rules.ValidationRule{ Rules.Sometimes, Rules.Regex(`09\d{9}`), Rules.Unique("phone", "users") },
+        },
+        {
+            FieldName: "password",
+            Rules: []Rules.ValidationRule{ Rules.Sometimes, Rules.LengthBetween(8, 16) },
+        },
+    })
 }
