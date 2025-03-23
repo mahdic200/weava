ea21dda6566a5cc1504f7c38d29a74ec59e37425
mahdic200 <rmahdi170@gmail.com>
2025-03-23 06:15:28 +0330

    Added UserController Show method

---
+++ Controllers/Admin/UserController/Show.go
@@ -0,0 +1,26 @@
+package UserController
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/mahdic200/weava/Config"
+	"github.com/mahdic200/weava/Models"
+	"github.com/mahdic200/weava/Models/User"
+	"github.com/mahdic200/weava/Utils/Http"
+)
+
+func Show(c *fiber.Ctx) error {
+	id, id_err := Http.IdParamValidator(c)
+	if id_err != nil {
+		return c.Status(200).JSON(id_err)
+	}
+	var user Models.User
+	User.Find(Config.DB, id, &user)
+	if user.Id == 0 {
+		return c.Status(404).JSON(fiber.Map{
+			"message": "User not found",
+		})
+	}
+	return c.Status(200).JSON(fiber.Map{
+		"data": user,
+	})
+}
