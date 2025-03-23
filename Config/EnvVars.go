d2197ed51129cb11e180d36344249ef60d3e501f
mahdic200 <rmahdi170@gmail.com>
2025-03-23 04:05:34 +0330

    Updated Config/EnvVars

--- Config/EnvVars.go
+++ Config/EnvVars.go
@@ -13,6 +13,7 @@ type EnvVarsStructure struct {
 	DB_NAME     string
 	DB_USER     string
 	DB_PASSWORD string
+	TIMEZONE    string
 }
 
 func GetEnv() (*EnvVarsStructure, error) {
@@ -21,10 +22,11 @@ func GetEnv() (*EnvVarsStructure, error) {
 	}
 	return &EnvVarsStructure{
 		APP_BASEURL: os.Getenv("APP_BASEURL"),
-		DB_HOST: os.Getenv("DB_HOST"),
-		DB_PORT: os.Getenv("DB_PORT"),
-		DB_NAME: os.Getenv("DB_NAME"),
-		DB_USER: os.Getenv("DB_USER"),
+		DB_HOST:     os.Getenv("DB_HOST"),
+		DB_PORT:     os.Getenv("DB_PORT"),
+		DB_NAME:     os.Getenv("DB_NAME"),
+		DB_USER:     os.Getenv("DB_USER"),
 		DB_PASSWORD: os.Getenv("DB_PASSWORD"),
+		TIMEZONE: os.Getenv("TIMEZONE"),
 	}, nil
 }
