6e35a1d1d857a73924432957fce56fe0e2267a6a
mahdic200 <rmahdi170@gmail.com>
2025-03-23 04:29:02 +0330

    Applied gofiber to project

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/mahdic200/weava/Config"
+)
 
 func main() {
-	fmt.Printf("Welcome to Weava\n")
+	app := fiber.New()
+
+	if err := Config.Connect(); err != nil {
+		fmt.Printf("Could not connect to the database\n")
+		fmt.Printf("%v\n", err.Error())
+		os.Exit(2)
+	}
+
+	log.Fatal(app.Listen(":8000"))
 }
