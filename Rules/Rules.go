bc4dda1e2eaae4b7274699ce208e44f608f40ddf
mahdic200 <rmahdi170@gmail.com>
2025-03-23 05:32:57 +0330

    Added Rules

---
+++ Rules/Rules.go
@@ -0,0 +1,16 @@
+package Rules
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+type ValidationRule func(c *fiber.Ctx, field_name string) (passed bool, message string, flags *Flags, err error)
+
+type FieldRules struct {
+	FieldName string
+	Rules     []ValidationRule
+}
+
+type Flags struct {
+	IsNull bool
+}
