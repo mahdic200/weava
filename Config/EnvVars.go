54d6d3bc25129ea0d8a781b136fc63c74772b239
mahdic200 <rmahdi170@gmail.com>
2025-03-23 03:57:29 +0330

    Updated GetEnv in Config package

--- Config/EnvVars.go
+++ Config/EnvVars.go
@@ -8,6 +8,11 @@ import (
 
 type EnvVarsStructure struct {
 	APP_BASEURL string
+	DB_HOST     string
+	DB_PORT     string
+	DB_NAME     string
+	DB_USER     string
+	DB_PASSWORD string
 }
 
 func GetEnv() (*EnvVarsStructure, error) {
@@ -16,5 +21,10 @@ func GetEnv() (*EnvVarsStructure, error) {
 	}
 	return &EnvVarsStructure{
 		APP_BASEURL: os.Getenv("APP_BASEURL"),
+		DB_HOST: os.Getenv("DB_HOST"),
+		DB_PORT: os.Getenv("DB_PORT"),
+		DB_NAME: os.Getenv("DB_NAME"),
+		DB_USER: os.Getenv("DB_USER"),
+		DB_PASSWORD: os.Getenv("DB_PASSWORD"),
 	}, nil
 }
