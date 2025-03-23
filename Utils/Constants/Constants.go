79f33e5bf6f016332fa968e09b4a1ff51fd456b0
mahdic200 <rmahdi170@gmail.com>
2025-03-23 04:44:22 +0330

    Added Constants util

---
+++ Utils/Constants/Constants.go
@@ -0,0 +1,20 @@
+package Constants
+
+import (
+	"os"
+	"path/filepath"
+)
+
+func GetBaseDir() string {
+	dir, err := os.Getwd()
+	if err != nil {
+		return ""
+	}
+	return dir
+}
+
+var BASE_DIR string = GetBaseDir()
+
+var PUBLIC_DIR string = filepath.Join(BASE_DIR, "public")
+
+var UPLOADS_PATH string = filepath.Join(PUBLIC_DIR, "uploads")
