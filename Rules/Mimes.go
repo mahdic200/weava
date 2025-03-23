16dff5c329080a09a3b409b3c9403ba35e049de1
mahdic200 <rmahdi170@gmail.com>
2025-03-23 05:57:02 +0330

    Added Mimes rule

---
+++ Rules/Mimes.go
@@ -0,0 +1,30 @@
+package Rules
+
+import (
+	"fmt"
+	"slices"
+	"strings"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/mahdic200/weava/Providers/Validation"
+	"github.com/mahdic200/weava/Services/FileService"
+)
+
+func Mimes(array ...string) ValidationRule {
+	return func(c *fiber.Ctx, field_name string) (passed bool, message string, flags *Flags, err error) {
+		value, err := c.FormFile(field_name)
+		if err != nil {
+			return false, "", nil, fmt.Errorf("error , Mines Rule : %s", err)
+		}
+		otherKeys := make(map[string]string)
+		otherKeys["{mimes}"] = strings.Join(array, " , ")
+		message, err = Validation.ErrorMessageProvider(field_name, "mimes", otherKeys)
+		if err != nil {
+			return false, "", nil, err
+		}
+		if !slices.Contains(array, FileService.GetFileExtension(value.Filename)) {
+			return false, message, nil, nil
+		}
+		return true, "", nil, nil
+	}
+}
