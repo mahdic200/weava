d6b28f40df40f204bd3fff7585ea0db5600e21b9
mahdic200 <rmahdi170@gmail.com>
2025-03-23 06:17:20 +0330

    Added UserController Store method

---
+++ Controllers/Admin/UserController/Store.go
@@ -0,0 +1,76 @@
+package UserController
+
+import (
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/mahdic200/weava/Config"
+	"github.com/mahdic200/weava/Models/User"
+	"github.com/mahdic200/weava/Services/FileService"
+	"github.com/mahdic200/weava/Utils"
+	"github.com/mahdic200/weava/Utils/Http"
+)
+
+func Store(c *fiber.Ctx) error {
+	tx := Config.DB.Begin()
+	defer func() {
+		if r := recover(); r != nil {
+			tx.Rollback()
+		}
+	}()
+	if tx.Error != nil {
+		return c.Status(500).JSON(fiber.Map{
+			"message": "Internal server error",
+		})
+	}
+	/* Parsing body */
+	data, err := Http.BodyParser(c)
+	if err != nil {
+		tx.Rollback()
+		return c.Status(400).JSON(fiber.Map{
+			"message": "Invalid data",
+		})
+	}
+	file, err := c.FormFile("image")
+	if err != nil {
+		tx.Rollback()
+		return c.Status(500).JSON(fiber.Map{
+			"message": "Failed to get the file!",
+		})
+	}
+
+	fs := FileService.New(file)
+
+	if err := fs.SaveToPublic("uploads", "images", "user-profiles"); err != nil {
+		tx.Rollback()
+		return c.Status(500).JSON(fiber.Map{
+			"message": "Failed to save file !",
+		})
+	}
+	data["image"] = fs.GetRelativePath()
+
+	data["password"], err = Utils.GenerateHashPassword(data["password"])
+	if err != nil {
+		tx.Rollback()
+		return c.Status(500).JSON(fiber.Map{
+			"message": "Internal server error",
+		})
+	}
+	if err := User.Create(tx, data).Error; err != nil {
+		tx.Rollback()
+		os.Remove(fs.GetFinalPath())
+		return c.Status(500).JSON(fiber.Map{
+			"message": "Internal server error !",
+		})
+	}
+	if err := tx.Commit().Error; err != nil {
+		tx.Rollback()
+		os.Remove(fs.GetFinalPath())
+		return c.Status(500).JSON(fiber.Map{
+			"message": "Internal server error",
+		})
+	}
+	return c.Status(200).JSON(fiber.Map{
+		"message": "user created successfully !",
+	})
+}
