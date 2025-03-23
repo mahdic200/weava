d5c553c697ec9d375af213c81c6c648a30db1a75
mahdic200 <rmahdi170@gmail.com>
2025-03-23 06:14:46 +0330

    Added user index route

--- Routes/Routes.go
+++ Routes/Routes.go
@@ -1,8 +1,17 @@
 package Routes
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/mahdic200/weava/Controllers/Admin/UserController"
+)
 
 func SetupRoutes(app *fiber.App) {
+
+    adminGroup := app.Group("/admin")
+
+    userGroup := adminGroup.Group("/user")
+    userGroup.Get("/", UserController.Index).Name("admin.user.index")
+
     /* Static file rendering */
     app.Static("/", "public")
 
