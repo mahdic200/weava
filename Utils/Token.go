fdbeef7905ff39ddfbe0470c8d9438a914725585
mahdic200 <rmahdi170@gmail.com>
2025-03-30 19:03:31 +0330

    Updated Token util

--- Utils/Token.go
+++ Utils/Token.go
@@ -9,34 +9,36 @@ import (
 )
 
 type JWTClaims struct {
-	jwt.Claims
-	entity_id       int
-	table_name      string
-	expiration_date time.Time
+	Entity_id       int       `json:"entity_id"`
+	Table_name      string    `json:"table_name"`
+	Expiration_date time.Time `json:"expiration_date"`
+	jwt.RegisteredClaims
 }
 
-func CreateToken(entity_id int64, table_name string, remember_me bool) (string, error) {
+func CreateToken(entity_id int64, table_name string, remember_me bool) (string, time.Time, error) {
 	months := 1
 	if remember_me {
 		months = 6
 	}
 
+	expire_time := time.Now().AddDate(0, months, 0)
+
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"entity_id":       entity_id,
 		"table_name":      table_name,
-		"expiration_date": time.Now().AddDate(0, months, 0).Unix(),
+		"expiration_date": expire_time,
 	})
 
 	tokenString, err := token.SignedString([]byte(Config.JWT_KEY))
 
 	if err != nil {
-		return "", err
+		return "", time.Time{}, err
 	}
-	return string(tokenString), nil
+	return string(tokenString), expire_time, nil
 }
 
 func VerifyToken(tokenString string) (int64, string, error) {
-	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &JWTClaims{}, func(token *jwt.Token) (any, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
 		}
@@ -46,15 +48,14 @@ func VerifyToken(tokenString string) (int64, string, error) {
 	if err != nil {
 		return 0, "", fmt.Errorf("invalid token")
 	}
-
-	claims, ok := token.Claims.(JWTClaims)
+	claims, ok := token.Claims.(*JWTClaims)
 	if !ok {
 		return 0, "", fmt.Errorf("invalid token")
 	}
 
-	if claims.expiration_date.Unix() < time.Now().Unix() {
+	if claims.Expiration_date.Unix() < time.Now().Unix() {
 		return 0, "", fmt.Errorf("expired token")
 	}
 
-	return int64(claims.entity_id), claims.table_name, nil
+	return int64(claims.Entity_id), claims.Table_name, nil
 }
