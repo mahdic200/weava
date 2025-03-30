4dbd42df6957d7b8779108c1ef108accaa933dc3
mahdic200 <rmahdi170@gmail.com>
2025-03-30 19:25:17 +0330

    Added seed command

---
+++ cmd/seed.go
@@ -0,0 +1,39 @@
+package cmd
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/mahdic200/weava/Config"
+	"github.com/mahdic200/weava/Models/User"
+	"github.com/mahdic200/weava/Utils"
+	"github.com/spf13/cobra"
+)
+
+// seedCmd represents the seed command
+var seedCmd = &cobra.Command{
+	Use:   "seed",
+	Short: "Seeds the database",
+	Long:  `Seeds the database`,
+	Run: func(cmd *cobra.Command, args []string) {
+		tx := Config.DB
+		pass, _ := Utils.GenerateHashPassword("password")
+		if err := User.Create(tx, map[string]string{"first_name": "admin", "email": "admin@gmail.com", "phone": "09531532475", "password": pass, "created_at": time.Now().String()}); err != nil {
+			fmt.Printf("Seeded the database successfully\n")
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(seedCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// seedCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// seedCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
