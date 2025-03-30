8e01d4b7cd4fe256b9e1bc7bf60acd6f84232248
mahdic200 <rmahdi170@gmail.com>
2025-03-30 19:05:15 +0330

    Updated ErrorProvider

--- Providers/ErrorProvider.go
+++ Providers/ErrorProvider.go
@@ -1,15 +1,13 @@
 package Providers
 
 import (
-	"fmt"
-
 	"github.com/mahdic200/weava/Config"
 )
 
-func ErrorProvider(err error) error {
+func ErrorProvider(err error) string {
 	message := err.Error()
 	if !Config.APP_DEBUG {
 		message = "Internal server error"
 	}
-	return fmt.Errorf("%s", message)
+	return message
 }
