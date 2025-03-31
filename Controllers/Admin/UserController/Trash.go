23319ba965af62c918d3fcb1a26c494e612a02f6
mahdic200 <rmahdi170@gmail.com>
2025-03-31 23:43:27 +0330

    Added UserController Trash method

---
+++ Controllers/Admin/UserController/Trash.go
@@ -0,0 +1,24 @@
+package UserController
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/mahdic200/weava/Config"
+	"github.com/mahdic200/weava/Models"
+	"github.com/mahdic200/weava/Models/User"
+	"github.com/mahdic200/weava/Resources/UserResource"
+	"github.com/mahdic200/weava/Utils/Http"
+)
+
+func Trash(c *fiber.Ctx) error {
+	var users []Models.User
+	tx := Config.DB.Table("users").Where("deleted_at IS NOT NULL")
+	tx.Order("id")
+
+	var metadata Http.PaginationMetadata
+	tx, metadata = User.Paginate(tx, c)
+	tx.Find(&users)
+	return c.Status(200).JSON(fiber.Map{
+		"data":     UserResource.Collection(users),
+		"metadata": metadata,
+	})
+}
