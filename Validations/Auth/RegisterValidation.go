8c2cb5e722c88f7b112069d464a276f9c17694aa
mahdic200 <rmahdi170@gmail.com>
2025-04-02 20:38:21 +0330

    Updated RegisterValidation

--- Validations/Auth/RegisterValidation.go
+++ Validations/Auth/RegisterValidation.go
@@ -6,8 +6,27 @@ import (
 	"github.com/mahdic200/weava/Rules"
 )
 
-func Store() func(c *fiber.Ctx) error {
+func Register() func(c *fiber.Ctx) error {
 	return Middlewares.ValidationMiddleware([]Rules.FieldRules{
-		// !TODO
+		{
+			FieldName: "first_name",
+			Rules:     []Rules.ValidationRule{Rules.Required, Rules.LengthBetween(3, 255)},
+		},
+		{
+			FieldName: "last_name",
+			Rules:     []Rules.ValidationRule{Rules.Sometimes, Rules.LengthBetween(3, 255)},
+		},
+		{
+			FieldName: "email",
+			Rules:     []Rules.ValidationRule{Rules.Required, Rules.Email, Rules.MaxLength(255), Rules.Unique("email", "users")},
+		},
+		{
+			FieldName: "phone",
+			Rules:     []Rules.ValidationRule{Rules.Required, Rules.Regex(`09\d{9}`), Rules.Unique("phone", "users")},
+		},
+		{
+			FieldName: "password",
+			Rules:     []Rules.ValidationRule{Rules.Required, Rules.LengthBetween(8, 16)},
+		},
 	})
 }
