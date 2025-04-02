127e6821c4b59a5290b1c8ae4141b636e2780307
mahdic200 <rmahdi170@gmail.com>
2025-04-02 20:37:04 +0330

    Updated session model

--- Models/Session.go
+++ Models/Session.go
@@ -7,7 +7,7 @@ import (
 type Session struct {
 	Id         uint       `json:"id" gorm:"not null;primaryKey"`
 	User_id    uint       `json:"user_id" gorm:"not null;primaryKey"`
-	User       *User      `json:"user" gorm:"foreignKey:User_id"`
+	User       *User      `json:"user" gorm:"foreignKey:User_id;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
 	Created_at *time.Time `json:"created_at"`
 	Updated_at *time.Time `json:"updated_at"`
 }
