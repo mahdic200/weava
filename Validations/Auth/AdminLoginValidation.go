9e457951827a5f4b7ac06a1f18c33b6a9a295ba8
mahdic200 <rmahdi170@gmail.com>
2025-04-09 21:12:30 +0330

    Added AdminLoginValidation

---
+++ Validations/Auth/AdminLoginValidation.go
@@ -0,0 +1,20 @@
+package Auth
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/mahdic200/weava/Middlewares"
+	"github.com/mahdic200/weava/Rules"
+)
+
+func AdminLogin() func(c *fiber.Ctx) error {
+	return Middlewares.ValidationMiddleware([]Rules.FieldRules{
+		{
+			FieldName: "email",
+			Rules:     []Rules.ValidationRule{Rules.Required, Rules.Email},
+		},
+		{
+			FieldName: "password",
+			Rules:     []Rules.ValidationRule{Rules.Required, Rules.MinLength(8)},
+		},
+	})
+}
