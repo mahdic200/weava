cb4c0b6fda73419ee949712d24a2516c0b6835c7
mahdic200 <rmahdi170@gmail.com>
2025-05-23 19:34:28 +0330

    Updated main.go

--- main.go
+++ main.go
@@ -13,11 +13,11 @@ func main() {
 	/* These two if sections are so important, as much as if they fail ,
 	application must exit */
 	if err := Config.GetEnv(); err != nil {
-		fmt.Printf("Config, connect : %s\n", err.Error())
+		fmt.Printf("Runtime Error: Could not load environment variables : %s\n", err.Error())
 		os.Exit(2)
 	}
 	if err := Config.Connect(); err != nil {
-		fmt.Printf("Could not connect to the database\n")
+		fmt.Printf("Connection Error : Could not connect to the database\n")
 		fmt.Printf("%v\n", err.Error())
 		os.Exit(2)
 	}
