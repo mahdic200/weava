7c3a1cda07c4c4b362df59f5148d03a108e9f626
mahdic200 <rmahdi170@gmail.com>
2025-06-27 01:01:04 +0330

    Updated AdminSessions

--- Models/AdminSession.go
+++ Models/AdminSession.go
@@ -5,9 +5,10 @@ import (
 )
 
 type AdminSession struct {
-	Id         uint       `json:"id" gorm:"not null;primaryKey"`
-	Admin_id   uint       `json:"admin_id" gorm:"not null;primaryKey"`
-	Admin      *Admin     `json:"admin" gorm:"foreignKey:Admin_id;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
-	Created_at *time.Time `json:"created_at"`
-	Updated_at *time.Time `json:"updated_at"`
+	Id           uint       `json:"id" gorm:"not null;primaryKey"`
+	Token_string string     `json:"token_string"`
+	Admin_id     uint       `json:"admin_id" gorm:"not null;primaryKey"`
+	Admin        *Admin     `json:"admin" gorm:"foreignKey:Admin_id;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	Created_at   *time.Time `json:"created_at"`
+	Updated_at   *time.Time `json:"updated_at"`
 }
